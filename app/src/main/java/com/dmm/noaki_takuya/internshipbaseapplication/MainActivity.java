b74bccaaa8a2a6686c99440d88e3d2d625a711b4
nokkii <nokkii.03@gmail.com>
2017-09-27 07:38:40 +0900

    Initialize for Version 4.4

---
+++ app/src/main/java/com/dmm/noaki_takuya/internshipbaseapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dmm.noaki_takuya.internshipbaseapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
