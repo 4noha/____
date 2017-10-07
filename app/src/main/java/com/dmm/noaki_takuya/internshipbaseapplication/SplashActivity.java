281808d4e84e164dcaf70423293bb8fc0fc1b0a5
nokkii <nokkii.03@gmail.com>
2017-10-07 23:22:11 +0900

    Add Title layout

--- app/src/main/java/com/dmm/noaki_takuya/internshipbaseapplication/SplashActivity.java
+++ app/src/main/java/com/dmm/noaki_takuya/internshipbaseapplication/SplashActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 /**
  * Created by michiko on 10/7/17.
@@ -21,14 +22,17 @@ public class SplashActivity extends AppCompatActivity {
         activity=this;
         activity.setContentView(R.layout.activity_main);
 
-        ImageView top= (ImageView) findViewById(R.id.top_image);
 
-        animateAlpha(top);
+        //ImageView top = (ImageView) findViewById(R.id.top_image);
+        RelativeLayout splashLayout = (RelativeLayout) activity.findViewById(R.id.splash_layout);
+
+        animateAlpha(splashLayout);
 
         Handler hdl = new Handler();
         // 第２引数で切り替わる秒数(ミリ秒)を指定、今回は2秒
         hdl.postDelayed(new splashHandler(), 3000);
     }
+
     class splashHandler implements Runnable {
         public void run() {
             Intent intent = new Intent(activity, ChoiceHouseActivity.class);
@@ -42,13 +46,13 @@ public class SplashActivity extends AppCompatActivity {
      *
      * @param target
      */
-    private void animateAlpha( ImageView target ) {
+    private void animateAlpha(RelativeLayout target) {// ImageView target ) {
 
         // alphaプロパティを0fから1fに変化させます
         ObjectAnimator objectAnimator = ObjectAnimator.ofFloat( target, "alpha", 1f, 0f );
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         }
 
         catch (InterruptedException e){
@@ -56,7 +60,7 @@ public class SplashActivity extends AppCompatActivity {
         }
 
         // 3秒かけて実行させます
-        objectAnimator.setDuration( 3000 );
+        objectAnimator.setDuration( 2000 );
 
         // アニメーションを開始します
         objectAnimator.start();
